384ca41b97d359846dd210cc957c0cc29d282b0d
Ryan Hartley <ryanhartlee@gmail.com>
2019-06-14 23:13:27 -0700

    pulled html address

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# jobextension
+Example code for my article on building a simple Google Chrome extension.
--- content.js
+++ content.js
@@ -1,14 +1,6 @@
-// content.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = window.location.origin;
-      // $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
-      // This line is new!
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
-  }
-);
+// Send a message containing the page details back to the event page
+chrome.runtime.sendMessage({
+    'title': document.title,
+    'url': window.location.href,
+    'summary': window.getSelection().toString()
+});

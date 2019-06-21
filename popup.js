32ed19b6f1e2f21a41a20f23505f3d83bec02671
Ryan Hartley <ryanhartlee@gmail.com>
2019-06-21 16:27:19 -0700

    working through AuthenticJobs' API

--- popup.js
+++ popup.js
@@ -1,87 +1,70 @@
-// This callback function is called when the content script has been
-// injected and returned its results
-let company = 'nothing';
+const authenticJobsAPIKey = "84d108dcaeb8e00c51cc4ae1be33268d";
+let company = "nothing";
 
-function onPageDetailsReceived(pageDetails)  {
-    document.getElementById('title').value = pageDetails.title;
-    document.getElementById('url').value = pageDetails.url;
-    document.getElementById('summary').innerText = pageDetails.summary;
+let results = [];
 
-    let site = pageDetails.url;
-    site = site.replace('https://', '');
-    site = site.replace('http://', '');
-    site = site.replace('www.', '');
-    site = site.substring(0, site.indexOf('.com'));
+let authJobs = function() {
+  fetch(
+    "https://authenticjobs.com/api/?api_key=" +
+      authenticJobsAPIKey +
+      "&method=aj.jobs.search&company=" +
+      company +
+      "&format=json"
+  )
+    // .then(function(response) {
+    //   alert(response);
+    // })
+    .then(function(response) {
+      response.json().then(function(data) {
+        if (data.listings.total == 0) {
+          document.getElementById("resultTarget").innerHTML +=
+            "<div class='jobResultLine'><h5>No Jobs Found</h5></div><br>";
+        } else {
+          // results = JSON.stringify(data.listings.listing);
+          for (let i = 0; i < 10; i++) {
+            document.getElementById("resultTarget").innerHTML +=
+              "<div class='jobResultLine' id='job'" +
+              i +
+              "><h5>" +
+              data.listings.listing[i].title +
+              "</h5><a href=" +
+              data.listings.listing[i].url +
+              " target='_blank'>Link</a></div><br>";
+          }
+        }
+        // alert(results)
+      });
+    });
+};
 
-    company = site;
-    document.getElementById('companyName').innerHTML = company;
+function onPageDetailsReceived(pageDetails) {
+  let site = pageDetails.url;
+  site = site.replace("https://", "");
+  site = site.replace("http://", "");
+  site = site.replace("www.", "");
+  site = site.substring(0, site.indexOf(".com"));
+
+  company = site;
+  document.getElementById("companyName").innerHTML = company;
+  authJobs();
 }
 
 // Global reference to the status display SPAN
 var statusDisplay = null;
 
-// POST the data to the server using XMLHttpRequest
-// function addBookmark() {
-//     // Cancel the form submit
-//     event.preventDefault();
-//     alert('addBookmark')
-//     // The URL to POST our data to
-//     var postUrl = 'http://post-test.local.com';
-//
-//     // Set up an asynchronous AJAX POST request
-//     var xhr = new XMLHttpRequest();
-//     xhr.open('POST', postUrl, true);
-//
-//     // Prepare the data to be POSTed by URLEncoding each field's contents
-//     var title = encodeURIComponent(document.getElementById('title').value);
-//     var url = encodeURIComponent(document.getElementById('url').value);
-//     var summary = encodeURIComponent(document.getElementById('summary').value);
-//     var tags = encodeURIComponent(document.getElementById('tags').value);
-//
-//     var params = 'title=' + title +
-//                  '&url=' + url +
-//                  '&summary=' + summary +
-//                  '&tags=' + tags;
-//
-//     // Replace any instances of the URLEncoded space char with +
-//     params = params.replace(/%20/g, '+');
-//
-//     // Set correct header for form data
-//     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-//
-//     // Handle request state change events
-//     xhr.onreadystatechange = function() {
-//         // If the request completed
-//         if (xhr.readyState == 4) {
-//             statusDisplay.innerHTML = '';
-//             if (xhr.status == 200) {
-//                 // If it was a success, close the popup after a short delay
-//                 statusDisplay.innerHTML = 'Saved!';
-//                 window.setTimeout(window.close, 1000);
-//             } else {
-//                 // Show what went wrong
-//                 statusDisplay.innerHTML = 'Error saving: ' + xhr.statusText;
-//             }
-//         }
-//     };
-//
-//     // Send the request and set status
-//     xhr.send(params);
-//     statusDisplay.innerHTML = 'Saving...';
-// }
-
 // When the popup HTML has loaded
-window.addEventListener('load', function(evt) {
-    // Cache a reference to the status display SPAN
-    statusDisplay = document.getElementById('status-display');
+window.addEventListener("load", function(evt) {
+  // Cache a reference to the status display SPAN
+  statusDisplay = document.getElementById("status-display");
 
-    // Handle the bookmark form submit event with our addBookmark function
-    // document.getElementById('addbookmark').addEventListener('submit', addBookmark);
-    // Get the event page
-    chrome.runtime.getBackgroundPage(function(eventPage) {
-        // Call the getPageInfo function in the event page, passing in
-        // our onPageDetailsReceived function as the callback. This injects
-        // content.js into the current tab's HTML
-        eventPage.getPageDetails(onPageDetailsReceived);
-    });
+  // Handle the bookmark form submit event with our addBookmark function
+  // document.getElementById('addbookmark').addEventListener('submit', addBookmark);
+  // Get the event page
+  chrome.runtime.getBackgroundPage(function(eventPage) {
+    // Call the getPageInfo function in the event page, passing in
+    // our onPageDetailsReceived function as the callback. This injects
+    // content.js into the current tab's HTML
+    eventPage.getPageDetails(onPageDetailsReceived);
+  });
+  // authJobs();
 });
